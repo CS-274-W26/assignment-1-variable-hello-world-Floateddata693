e1070634fc76ac637f08503e70b62572978f23e0
Michael Lyons <lyonmich@flip3.engr.oregonstate.edu>
2026-01-19 19:19:21 -0800

    done

--- prog1.c
+++ prog1.c
@@ -1,11 +1,26 @@
-#include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+srand(time(NULL));
+int myrand = rand() % 11;
+
+printf("The random number was: %d\n" , myrand);
+
+if(myrand <= 4){
+printf("Eat more beef, kick less cats \n");
 }
+
+else if(myrand <= 9){
+printf("FRODO LIVES\n");
+}
+
+else{
+printf("Larn is the best roguelike \n");
+}
+
+
+
+}
+
